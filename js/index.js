92020d10e6f6b04c8329002355f0dfe7fb11ea19
Sarah Berri <sarahberri@outlook.fr>
2023-06-06 02:44:59 +0200

    add

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-// Sélectionnez l'icône du menu et l'élément du menu déroulant
 const menuIcon = document.querySelector('.header .icon');
 const navbarItems = document.querySelector('.header .navbar-items');
 
-// Ajoutez un écouteur d'événement de clic à l'icône du menu
 menuIcon.addEventListener('click', function () {
   navbarItems.classList.toggle('active'); // Ajoute ou retire la classe .active
 });
@@ -17,25 +15,20 @@ const carouselChildrens = [...carousel.children];
 
 let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 
-// Get the number of cards that can fit in the carousel at once
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
 carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
   carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
 
-// Insert copies of the first few cards to end of carousel for infinite scrolling
 carouselChildrens.slice(0, cardPerView).forEach(card => {
   carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
 carousel.classList.add("no-transition");
 carousel.scrollLeft = carousel.offsetWidth;
 carousel.classList.remove("no-transition");
 
-// Add event listeners for the arrow buttons to scroll the carousel left and right
 arrowBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
@@ -51,8 +44,7 @@ const dragStart = (e) => {
 }
 
 const dragging = (e) => {
-  if (!isDragging) return; // if isDragging is false return from here
-  // Updates the scroll position of the carousel based on the cursor movement
+  if (!isDragging) return;
   carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
 }
 
@@ -62,7 +54,6 @@ const dragStop = () => {
 }
 
 const infiniteScroll = () => {
-  // If the carousel is at the beginning, scroll to the end
   if (carousel.scrollLeft === 0) {
     carousel.classList.add("no-transition");
     carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
@@ -75,15 +66,11 @@ const infiniteScroll = () => {
     carousel.classList.remove("no-transition");
   }
 
-  // Fait défiler le carousel d'un card width à la fois
   carousel.scrollLeft += firstCardWidth;
-  // Appelle la fonction de défilement automatique après un délai de 3 secondes
   timeoutId = setTimeout(infiniteScroll, 3000);
 
-  // Clear existing timeout & start autoplay if mouse is not hovering over carousel
   clearTimeout(timeoutId);
   if (!wrapper.matches(":hover")) {
-    // Fait défiler le carousel automatiquement
     timeoutId = setTimeout(infiniteScroll, 3000);
   }
 }
@@ -91,14 +78,12 @@ const infiniteScroll = () => {
 const cardIcons = document.querySelectorAll(".card-icons i");
 
 cardIcons.forEach(icon => {
-  // Empêche le défilement du carousel lorsqu'un utilisateur clique sur une icône dans la classe "card-icons"
   icon.addEventListener("click", (event) => {
     event.stopPropagation();
   });
 });
 
 carousel.addEventListener("click", (event) => {
-  // Vérifie si l'élément cliqué est une icône de la classe "media-icons"
   if (event.target.classList.contains("media-icons")) {
     return;
   }
@@ -106,25 +91,17 @@ carousel.addEventListener("click", (event) => {
 
 
 
-// Attendre que toutes les ressources soient chargées
-window.addEventListener('load', function() {
-  // Sélectionner les éléments avec les images différées
+window.addEventListener('load', function () {
   var deferredImages = document.querySelectorAll('.deferred-image');
 
-  // Parcourir chaque élément
-  deferredImages.forEach(function(element) {
-    // Obtenir l'URL ou le chemin d'accès de l'image différée
+  deferredImages.forEach(function (element) {
     var imageUrl = element.getAttribute('data-image-url');
 
-    // Créer un nouvel élément d'image
     var image = new Image();
     image.src = imageUrl;
 
-    // Masquer l'image ou la positionner hors de l'écran
-    // en ajoutant une classe CSS appropriée
     image.classList.add('hidden');
 
-    // Insérer l'image dans le DOM, par exemple en le remplaçant par l'élément de placeholder
     element.parentNode.replaceChild(image, element);
   });
 });
